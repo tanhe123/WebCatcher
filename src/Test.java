fd75e9c26b866df1a1a546d27976e8b576e18999
Tanhehe <443016215@qq.com>
2013-10-20 20:46:41 +0800

    fix

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,34 @@
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-
 public class Test {
 	public static void main(String[] args) {
 		try {
+			String s = WebCatcher.getContent(new URL("http://www.shicimingju.com/1368.html"));
+			System.out.println(s);
+			
 			List<String> list = WebCatcher.getPoem(new URL("http://www.shicimingju.com/1368.html"));
 			for (String e : list) {
 				System.out.println(e);
 			}
+			
 		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+//		try {
+//			List<String> list = WebCatcher.getPoem(new URL("http://www.shicimingju.com/1368.html"));
+	//		for (String e : list) {
+		//		System.out.println(e);
+//			}
+//		} catch (MalformedURLException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
--- src/WebCatcher.java
+++ src/WebCatcher.java
@@ -9,36 +9,61 @@ import java.util.regex.Pattern;
 
 
 public class WebCatcher {
+	/**
+	 * get the content of the url, the default charset is utf-8
+	 * @param url the url of the content to get
+	 * @return the content of the url
+	 * @throws IOException
+	 */
+	public static String getContent(URL url) throws IOException {
+		return getContent(url, "UTF-8");
+	}
+	
+	/**
+	 * get the content of the url
+	 * @param url the url of the content to get
+	 * @param charsetName 网页的编码, such as gbk
+	 * @return the content of the url
+	 * @throws IOException
+	 */
+	public static String getContent(URL url, String charsetName) throws IOException {;
+		StringBuilder content = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), charsetName));
+		
+		String tmp;
+		while((tmp = reader.readLine()) != null) {
+			content.append(tmp);	
+			content.append("\n");
+		}
+		
+		reader.close();
+		
+		return content.toString();
+	}
 	
 	/**
 	 * using poemRegex to match the content of the url
 	 * @param url the url to patch, such as http://www.shicimingju.com/1368.html
 	 * @return list of the poems
+	 * @throws IOException 
 	 */
-	public static List<String> getPoem(URL url){
-		BufferedReader reader = null;
+	public static List<String> getPoem(URL url) throws IOException{
+		String content = getContent(url);
+		return getPoem(content);
+	}
+	
+	/**
+	 * using poemRegex to match the content
+	 * @param content content
+	 * @return list of the result 
+	 */
+	public static List<String> getPoem(String content) {
 		List<String> list = new ArrayList<String>();
 		
-		try {
-			//BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "gbk"));
-			reader = new BufferedReader(new InputStreamReader(url.openStream()));
-						
-			StringBuilder str = new StringBuilder();
-			String tmp;
-			while((tmp = reader.readLine()) != null) {
-				str.append(tmp);	
-				str.append("\n");
-			}
-			
-			reader.close();
-			
-			Pattern pattern = Pattern.compile(poemRegex);
-			Matcher mat = pattern.matcher(str);
-			while (mat.find()) {
-				list.add(mat.group(1));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		Pattern pattern = Pattern.compile(poemRegex);
+		Matcher mat = pattern.matcher(content);
+		while (mat.find()) {
+			list.add(mat.group(1));
 		}
 		
 		return list;
